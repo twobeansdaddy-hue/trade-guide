6300b68a6dbe3c5915727e5f55c1d9252c670929
TwoBeansDaddy <twobeansdaddy@gmail.com>
2026-07-09 17:03:13 +0900

    JUnit 테스트 생성

--- src/test/java/com/trade_guide/TradeGuideCalculatorTest.java
+++ src/test/java/com/trade_guide/TradeGuideCalculatorTest.java
@@ -8,11 +8,24 @@ public class TradeGuideCalculatorTest {
     @Test
     void 목표_매도가_이상이면_익절한다() {
         TradeGuideRequest request = new TradeGuideRequest(100, 120, 15, 8);
-
         TradeGuideCalculator calculator = new TradeGuideCalculator();
-
         TradeGuideResult result = calculator.calculate(request);
-
         assertEquals(TradeAction.TAKE_PROFIT, result.getTradeAction());
+    }
+
+    @Test
+    void 목표_매도가_이하면_매도한다() {
+        TradeGuideRequest request = new TradeGuideRequest(100, 90, 15, 8);
+        TradeGuideCalculator calculator = new TradeGuideCalculator();
+        TradeGuideResult result = calculator.calculate(request);
+        assertEquals(TradeAction.SELL, result.getTradeAction());
+    }
+
+    @Test
+    void 목표_매도가_사이면_보유한다() {
+        TradeGuideRequest request = new TradeGuideRequest(100, 100, 15, 8);
+        TradeGuideCalculator calculator = new TradeGuideCalculator();
+        TradeGuideResult result = calculator.calculate(request);
+        assertEquals(TradeAction.HOLD, result.getTradeAction());
     }
 }
