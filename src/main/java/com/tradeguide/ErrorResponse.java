930f1a1ce23851ea580b0a2cbe39bce8a3dc340e
TwoBeansDaddy <twobeansdaddy@gmail.com>
2026-07-28 11:07:55 +0900

    feat: API 예외 응답 처리 추가

--- src/main/java/com/tradeguide/ErrorResponse.java
+++ src/main/java/com/tradeguide/ErrorResponse.java
@@ -1,4 +1,13 @@
 package com.tradeguide;
 
 public class ErrorResponse {
+    private final String message;
+
+    public ErrorResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
--- src/main/java/com/tradeguide/GlobalExceptionHandler.java
+++ src/main/java/com/tradeguide/GlobalExceptionHandler.java
@@ -1,4 +1,22 @@
 package com.tradeguide;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(
+            IllegalArgumentException exception
+    ) {
+        ErrorResponse response = new ErrorResponse(exception.getMessage());
+
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(response);
+
+    }
 }
